447cc2d2388bf643a54b11aca91e89dcb3e0ed9d
Rian-rgb <rian3903@gmail.com>
2024-12-26 13:37:36 +0700

    task-8 add create table object_categories and replace name table query index in areas table

--- src/main/resources/db/migration/V2__create_object_categories_table.sql
+++ src/main/resources/db/migration/V2__create_object_categories_table.sql
@@ -1,15 +1,17 @@
-CREATE TABLE areas (
+CREATE TABLE object_categories (
     id RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
     name NVARCHAR2(100),
     is_active NUMBER(3),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    area_id RAW(16),
+    CONSTRAINT fk_area FOREIGN KEY (area_id) REFERENCES areas(id)
 );
 
 -- Index untuk kolom is_active
 CREATE INDEX idx_is_active
-ON areas (is_active);
+ON object_categories (is_active);
 
 -- Index untuk kolom name
 CREATE INDEX idx_name
-ON areas (name);
+ON object_categories (name);
