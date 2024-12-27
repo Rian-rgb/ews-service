0a7457d446d2bca9e85774b70980efe343be77fc
Rian-rgb <rian3903@gmail.com>
2024-12-27 11:41:21 +0700

    task-11 add migration key_processes

--- src/main/resources/db/migration/V1__create_areas_table.sql
+++ src/main/resources/db/migration/V1__create_areas_table.sql
@@ -1,15 +1,15 @@
 CREATE TABLE areas (
     id RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
-    name NVARCHAR2(100),
-    is_active NUMBER(3),
+    name NVARCHAR2(100) NOT NULL,
+    is_active NUMBER(3) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 -- Index untuk kolom is_active
-CREATE INDEX idx_is_active
+CREATE INDEX areas_is_active_index
 ON areas (is_active);
 
 -- Index untuk kolom name
-CREATE INDEX idx_name
+CREATE INDEX areas_name_index
 ON areas (name);
--- src/main/resources/db/migration/V3__create_key_processes_table.sql
+++ src/main/resources/db/migration/V3__create_key_processes_table.sql
@@ -1,17 +1,17 @@
-CREATE TABLE object_categories (
+CREATE TABLE key_processes (
     id RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
     name NVARCHAR2(100) NOT NULL,
     is_active NUMBER(3) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    area_id RAW(16),
-    CONSTRAINT fk_area FOREIGN KEY (area_id) REFERENCES areas(id)
+    object_category_id RAW(16),
+    CONSTRAINT fk_object_category FOREIGN KEY (object_category_id) REFERENCES object_categories(id)
 );
 
 -- Index untuk kolom is_active
-CREATE INDEX object_categories_is_active_index
-ON object_categories (is_active);
+CREATE INDEX key_processes_is_active_index
+ON key_processes (is_active);
 
 -- Index untuk kolom name
-CREATE INDEX object_categories_name_index
-ON object_categories (name);
+CREATE INDEX key_processes_name_index
+ON key_processes (name);
