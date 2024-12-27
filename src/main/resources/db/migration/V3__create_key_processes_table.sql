b83a5128c9a00b1e6e0734b3082fdce1f410d403
Rian-rgb <rian3903@gmail.com>
2024-12-27 13:06:27 +0700

    task-12 replace table areas, object_categories, key_processes column created_at and updated_at from not null to null

--- src/main/resources/db/migration/V3__create_key_processes_table.sql
+++ src/main/resources/db/migration/V3__create_key_processes_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE key_processes (
     id RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
     name NVARCHAR2(100) NOT NULL,
     is_active NUMBER(3) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     object_category_id RAW(16),
     CONSTRAINT fk_object_category FOREIGN KEY (object_category_id) REFERENCES object_categories(id)
 );
